78d66b60d73e67c86de25ea9f7a6d10c5eee7e05
osotnikov <alex28_501@hotmail.com>
2019-08-12 13:19:22 +0100

    fffff

--- FileUploadService.java
+++ FileUploadService.java
@@ -1,4 +1,4 @@
-package com.greensill.http.upload;
+package com.osotnikov.http.upload;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
