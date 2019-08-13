590e84123e100f96fc3a3bed2bb28d398e670d14
osotnikov <alex28_501@hotmail.com>
2019-08-13 14:17:45 +0100

    fff
    fffff

---
+++ StringUtils.java
@@ -0,0 +1,35 @@
+package com.greensill.string;
+
+public class StringUtils {
+
+    public static boolean equalsIgnoreWhitespace(String left, String right) {
+
+        if(left == right) {
+            return true;
+        } else if(left == null || right == null) {
+            return false;
+        } else {
+            left = left.replaceAll("\\s+","");
+            right = right.replaceAll("\\s+","");
+
+            return left.equals(right);
+        }
+    }
+
+    public static boolean equalsIgnoreWhitespaceApartFromNewlinesButTrim(String left, String right) {
+
+        if(left == right) {
+            return true;
+        } else if(left == null || right == null) {
+            return false;
+        } else {
+            left = left.replaceAll("\\r\\n","\n").trim();
+            right = right.replaceAll("\\r\\n","\n").trim();
+
+            left = left.replaceAll("[^\\S\\r\\n]+","");
+            right = right.replaceAll("[^\\S\\r\\n]+","");
+
+            return left.equals(right);
+        }
+    }
+}
